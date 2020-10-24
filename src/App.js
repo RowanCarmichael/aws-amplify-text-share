538f03502151772d4a6779e7363193a429a0cc8d
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2020-10-24 16:18:12 +1100

    Adding graphql API to get, create and delete notes with a description and optional image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API, Storage } from 'aws-amplify';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
 
 const App = () => {
-  const [text, setText] = useState('')
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
 
-  const handleTextChange = event => {
-    setText(event.target.value)
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    const notesFromAPI = apiData.data.listNotes.items;
+    await Promise.all(notesFromAPI.map(async note => {
+      if (note.image) {
+        const image = await Storage.get(note.image);
+        note.image = image;
+      }
+      return note;
+    }))
+    setNotes(apiData.data.listNotes.items);
   }
 
-  const copyToClipcoard = () => {
-    navigator.clipboard.writeText(text)
+  async function createNote() {
+    if (!formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
+  async function onFileChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
   }
   
   return (
     <div className="App">
-      <textarea value={text} onChange={handleTextChange} />
-      <button type="button" onClick={copyToClipcoard}>Copy</button>
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value })}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <input
+        type="file"
+        onChange={onFileChange}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id}>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+              {
+                note.image && <img src={note.image} style={{width: 400}} alt={note.description} />
+              }
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   )
